7ee592cf3e3e3c55abff610fb62e16b34a1031b5
dkeightley <derekjkeightley@gmail.com>
2019-02-20 09:12:31 +1100

    Add 'threaded=True' for Flask

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def index():
     return render_template('index.html', url=url), code
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0",port=80,threaded=True)
